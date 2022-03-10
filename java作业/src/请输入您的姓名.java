646e41659449ed2d92e80d1d298262f05d249820
2233xy <g2727958179@163.com>
2022-03-10 13:25:19 +0800

    。

---
+++ java作业/src/请输入您的姓名.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class 请输入您的姓名 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("请输入您的姓名");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println(name+"，您好！欢迎学习Java程序设计！");
+    }
+}
